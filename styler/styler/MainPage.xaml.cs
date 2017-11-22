83fe4c299648524fded7aa3338c83ae10f52a5dc
CodeOPS-3 <CodeOPS-3@AdminSoSoftsMBP.Dual-Band-Broadband-Modem>
2017-11-22 11:43:21 +0100

    first commit

---
+++ styler/styler/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace styler
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            //var location = locationPicker.Navigation;
+            //var style = stylePicker.Navigation;
+            var location = locationPicker.Items[locationPicker.SelectedIndex];
+
+            if (location == "Lekki")
+            {
+                await Navigation.PushAsync(new MapPage2());
+            }
+
+            else if (location == "Ikoyi")
+            {
+                await Navigation.PushAsync(new MapPage());
+            }
+            
+            
+        }
+    }
+}
---
+++ styler/styler/MapPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace styler
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+
+            var map = new Map(
+                MapSpan.FromCenterAndRadius(
+                    new Position(6.45, 3.433333), Distance.FromMiles(0.3)))
+            {
+                IsShowingUser = true,
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(map);
+            Content = stack;
+
+            var slider = new Slider(1, 18, 1);
+            slider.ValueChanged += (sender, e) =>
+            {
+                var zoomLevel = e.NewValue; // between 1 and 18
+                var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
+                map.MoveToRegion(new MapSpan(map.VisibleRegion.Center, latlongdegrees, latlongdegrees));
+            };
+
+            var position = new Position(6.45, 3.433333);
+            var pin = new Pin
+            {
+                Type = PinType.Place,
+                Position = position,
+                Label = "OutMall Plaza, Okorie Victor",
+                Address = "No. 54 Wole Olateju street, Lekki phase one, Lagos"
+            };
+            map.Pins.Add(pin);
+
+            var position2 = new Position(6.46, 3.601521);
+            var pin2 = new Pin
+            {
+                Type = PinType.Place,
+                Position = position2,
+                Label = "De-Shoppe, Chidinma Isiadinso",
+                Address = "No. 17 Bourdillon road, Ikoyi, Lagos"
+            };
+            map.Pins.Add(pin2);
+
+            pin.Clicked += Pin_Clicked;
+            pin2.Clicked += Pin_Clicked;
+        }
+
+        private void Pin_Clicked(object sender, EventArgs e)
+        {
+            var selectedPin = sender as Pin;
+
+            DisplayAlert(selectedPin?.Label, selectedPin?.Address, "Ok");
+        }
+    }
+}
